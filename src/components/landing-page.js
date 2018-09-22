50ccaced8334e83352c3669623bdbfeb80b06dbb
David <davidgraves831@gmail.com>
2018-09-21 20:21:08 -0700

    added screenshots

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -14,7 +14,7 @@ export function LandingPage(props) {
             <h1 className="welcome-title">Welcome to Calories R Gone</h1>
             <div className="welcome-body">
                 <p>This is a quick and easy app that allows you to log in your workout and see the calories that you burned.</p>
-                <p>To get a full expeirence of this app. Login using the username of "guest" and password of "password1"</p>
+                <p>To get a full expeirence of this app. Login using the username of "guest" and password of "password1!"</p>
             </div>
             </div>
 
